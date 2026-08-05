af226195b199db0e3f2f10b184c30d6e3ac966da
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-08-05 15:51:07 +0300

    Created HTML outline

--- front-end/to-do-project/src/App.jsx
+++ front-end/to-do-project/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-	return "Hello world";
+	return (
+		<>
+			<form>
+				<label htmlFor="newItem">New Item</label>
+				<input type="text" id="newItem" />
+				<button type="submit">Add</button>
+			</form>
+			<h1>Todo List</h1>
+			<ul>
+				<li>
+					<input type="checkbox" id="item" />
+					<label htmlFor="item">Item 1</label>
+					<button type="button">Delete</button>
+				</li>
+			</ul>
+		</>
+	);
 }
 
 export default App;
