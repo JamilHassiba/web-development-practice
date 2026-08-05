784d21382cbc0aa6030f0e3adcdec871f56f8272
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-08-06 00:31:11 +0300

    Store todos in local storage

--- front-end/to-do-project/src/App.jsx
+++ front-end/to-do-project/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import NewItemForm from "./components/NewItemForm";
 import TodoList from "./components/TodoList";
 
 function App() {
-	const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState(() => {
+		const data = localStorage.getItem("todos");
+		return data ? JSON.parse(data) : [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
 
 	function addTodo(title) {
 		setTodos((currentTodos) => [
