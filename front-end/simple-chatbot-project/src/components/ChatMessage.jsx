bbda5aef0e7d147cc128a7b976aaaee0d9999284
Jamil Hassiba <jamil.hassiba@student.manchester.ac.uk>
2026-08-07 01:11:19 +0300

    Styled chat messages

--- front-end/simple-chatbot-project/src/components/ChatMessage.jsx
+++ front-end/simple-chatbot-project/src/components/ChatMessage.jsx
@@ -3,11 +3,11 @@ import userImage from "../assets/user.png";
 
 function ChatMessage({ sender, message }) {
 	return (
-		<>
+		<div className="chat-message-container" sender={sender}>
 			{sender === "robot" && <img src={robotImage} alt="" />}
 			<div>{message}</div>
 			{sender === "user" && <img src={userImage} alt="" />}
-		</>
+		</div>
 	);
 }
 
